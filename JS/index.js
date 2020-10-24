cc4a6fc256b49fe72e3c6881067da3c48a9fd45d
Eldors <safonovpaspb@gmail.com>
2020-10-24 15:20:42 +0300

    Можно создавать новые колонки с заголовком

--- JS/index.js
+++ JS/index.js
@@ -1,139 +1,70 @@
 'use strict';
 
-let i = 0;
-
-const button = document.querySelector('button');
-const mainNotes = document.querySelector('.main-notes');
-
 const row = document.querySelector('.row');
-const column = document.querySelector('.column')
-const title = document.querySelector('.title');
-const addTheNote = document.querySelector('.add-the-note');
-const addColumn = document.querySelector('.add-column');
-const newColumn = document.querySelector('.new-column');
-/*
-function pushTheNote() {
-   const newNote = document.createElement('div'); 
-    newNote.className = 'note';
-    newNote.id = `0${i}`;
-    mainNotes.appendChild(newNote);
-    pusheTheTextarea();
-}*/
+
+function createFirstColumn () {
+    const firstColumn = document.createElement('div');
+    const firstAddColumnName = document.createElement('div');
+    const firstEnterName = document.createElement('input');
+
+    firstColumn.className = 'column';
+    firstAddColumnName.className = 'create-column';
+    firstEnterName.className = 'enter-name';
+    firstEnterName.setAttribute('type', 'text');
+    firstEnterName.setAttribute('value', 'добавить имя');
+    row.appendChild(firstColumn);
+    firstColumn.appendChild(firstAddColumnName);
+    firstAddColumnName.appendChild(firstEnterName);
+}
+
+createFirstColumn();
+
+function createAfterColumn () {
+    const afterColumn = document.createElement('div');
+    const addColumnName = document.createElement('div');
+    const enterName = document.createElement('input');
+
+    afterColumn.className = 'column';
+    addColumnName.className = 'create-column';
+    enterName.className = 'enter-name';
+    enterName.setAttribute('type', 'text');
+    enterName.setAttribute('value', 'добавить имя');
+    row.appendChild(afterColumn);
+    afterColumn.appendChild(addColumnName);
+    addColumnName.appendChild(enterName);
+}
 
 function addNewColum (event) {
-    if (!event.target.matches('div.new-column')) return
-
-    const addNameColumn = document.createElement('input');
-
-    addNameColumn.className = 'create-column';
-    addNameColumn.setAttribute('type', 'text');
-    
-    //newColumn.classList.add('none');
-    extinguishElement(newColumn);
-
-    addColumn.appendChild(addNameColumn).focus();
-   
-    addNameColumn.onblur = function() {
-
-        if(this.value == '') {
-            addColumn.removeChild(addNameColumn);
-            showElement(newColumn);
-           // newColumn.classList.remove('none');
-        } else {
-            const newTitle = document.createElement('div');
-            const title = document.createElement('span');
-
-            title.innerHTML = addNameColumn.value;
-
-            newTitle.className = 'title';
-           // title.value = addNameColumn.value;
-            addColumn.removeChild(addNameColumn);
-            addColumn.removeChild(newColumn);
-            addColumn.className = 'column';
-
-            addColumn.appendChild(newTitle);
-            newTitle.appendChild(title);
-           
-        }
-    }
-
-    /*const tit = document.createElement('div');
-    const text = document.createElement('textarea');
-    text.className = 'text';
-    tit.className = 'title';
-    text.setAttribute('autofocus', '')
-    const allColumn = document.querySelectorAll('.column');
-    const lastColumn = allColumn[allColumn.length - 1];
-    
-    lastColumn.appendChild(tit);
-    
-    tit.appendChild(text);
-
-    
-    addNote(lastColumn);
-   const column = document.createElement('div');
-    column.className = 'column';
-
-    row.insertBefore(column, addColumn);
-    addTitle();*/
-}
-
-function extinguishElement (elem) {
-    elem.classList.add('none');
-}
-
-function showElement (elem) {
-    elem.classList.remove('none');
-}
-
-function addTitle () {
-    const tit = document.createElement('div');
-    const text = document.createElement('textarea');
-    text.className = 'text';
-    tit.className = 'title';
-    text.setAttribute('autofocus', '')
-    const allColumn = document.querySelectorAll('.column');
-    const lastColumn = allColumn[allColumn.length - 1];
-    
-    lastColumn.appendChild(tit);
-    
-    tit.appendChild(text);
-
-    
-    addNote(lastColumn);
-
-}
-
-function addNote (a) {
-    if (a != undefined) {
-        const addnote = document.createElement('div');
-        addnote.className = 'note';
-
-    }   
-}
-
-function pusheTheTextarea () {
-    const k = document.getElementById(`0${i}`);
-   
-    const editorText = document.createElement('textarea');
-    
-    editorText.setAttribute('type', 'text');
-    editorText.setAttribute('autofocus', '');
-    editorText.setAttribute('maxlength', '54');
-    k.appendChild(editorText); // переделать под textarea
-
-    i++;
-}
-
-function toggleDone (event) {  //проверка в какой элемент попапл курсор
-    if (!event.target.matches('div.note')) return  
     console.log('1');
-    
-} 
+    if (!event.target.matches('div.create-column')) return
+    const parentColumn = event.target.parentElement;
+    const firstCh = parentColumn.firstChild;
+    console.log('parentColumn: ', parentColumn);
+    const inputName = document.querySelector('.enter-name');
+    console.log('inputName: ', inputName);
+    inputName.focus();
+    inputName.value = '';
+    inputName.onblur = function() {
+        if(this.value == '') {
+            inputName.value = 'Введите название';
+        } else {
+            parentColumn.removeChild(firstCh);         
+            createTitle(inputName,parentColumn);
+            createAfterColumn();
+         }
+    }
+}
 
+function createTitle(elem, parelem) {
+    const newTitle = document.createElement('div');
+    const title = document.createElement('span');
 
+    title.innerHTML = elem.value;
 
-////mainNotes.addEventListener('click', toggleDone);
+    newTitle.className = 'title';
 
-//button.addEventListener('click', pushTheNote);
-addColumn.addEventListener('click', addNewColum);
+    parelem.appendChild(newTitle);
+    newTitle.appendChild(title);
+}
+
+row.addEventListener('click', addNewColum);
